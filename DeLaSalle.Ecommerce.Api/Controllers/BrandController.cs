2d24057dcb2f22601f130f290a50f1bdf5eda597
Fabián Muñoz Tavares <fabian.munoz@congresogto.gob.mx>
2022-11-23 19:20:17 -0600

    Commit

--- DeLaSalle.Ecommerce.Api/Controllers/BrandController.cs
+++ DeLaSalle.Ecommerce.Api/Controllers/BrandController.cs
@@ -1,108 +1,145 @@
-﻿using DeLaSalle.Ecommerce.Api.Repositories.Interfaces;
-using DeLaSalle.Ecommerce.Core.Http;
+﻿using DeLaSalle.Ecommerce.Api.Services.Interfaces;
 using DeLaSalle.Ecommerce.Core.Dto;
+using DeLaSalle.Ecommerce.Core.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using DeLaSalle.Ecommerce.Core.Entities;
+using System.Security.Claims;
 
 namespace DeLaSalle.Ecommerce.Api.Controllers
 {
+    /// <summary>
+    /// Proporciona los métodos para la gestión de marcas
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class BrandController : Controller
     {
+        #region Globales
+        private IBrandService _service;
+        private readonly IHttpContextAccessor _contextAccessor;
+        #endregion
 
-        private IBrandRepository _repository;
-
-        public BrandController(IBrandRepository repository)
+        #region Constructores
+        public BrandController(IBrandService service, IHttpContextAccessor contextAccessor)
         {
-            _repository = repository;
+            _service = service;
+            _contextAccessor = contextAccessor;
         }
+        #endregion
+
         #region Métodos
+        /// <summary>
+        /// Recupera el listado de marcas que no están eliminadas
+        /// </summary>
+        /// <returns>Lista de marcas</returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<Response<List<BrandDto>>>> GetBrands()
         {
             var res = new Response<List<BrandDto>>();
-            var lista = await _repository.GetAllAsync();
+            var lista = await _service.GetAllAsync();
 
-            if (lista.Any())
+            if (lista != null)
             {
-                res.Data = lista.Select(b => new BrandDto(b)).ToList();
+                res.Data = lista;
                 return Ok(res);
             }
             return NotFound(res);
         }
+        /// <summary>
+        /// Recupera el detalle de la marca especificada
+        /// </summary>
+        /// <param name="id">Identificador de marca</param>
+        /// <returns>Datos de la marca</returns>
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<Response<BrandDto>>> GetBrand(int id)
         {
             var res = new Response<BrandDto>();
-            var brand = await _repository.GetByIdAsync(id);
+            var brand = await _service.GetByIdAsync(id);
 
             if (brand != null)
             {
-                BrandDto dto = new(brand);
-                res.Data = dto;
+                res.Data = brand;
                 return Ok(res);
             }
+            res.Errors.Add("Marca no encontrada!");
             return NotFound(res);
         }
+        /// <summary>
+        /// Guarda un nuevo registro de marca
+        /// </summary>
+        /// <param name="dto">Datos de la nueva marca</param>
+        /// <returns>Datos de la marca creada</returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<Response<BrandDto>>> PostBrand([FromBody] BrandDto dto)
         {
             var res = new Response<BrandDto>();
-            Brand marca = new()
-            {
-                Name = dto.Name,
-                Description = dto.Description,
-                CreatedDate = DateTime.Now,
-                CreatedBy = Login,
-            };
-            marca = await _repository.SaveAsync(marca);
-            dto.Id = marca.Id;
+            dto = await _service.SaveAsync(dto);
             res.Data = dto;
 
             return CreatedAtAction("GetBrand", new { Id = 0 }, res);
         }
+        /// <summary>
+        /// Actualiza los datos de la marca
+        /// </summary>
+        /// <param name="dto">Datos a actualizar</param>
+        /// <returns>Respuesta vacía en caso de actualización exitosa</returns>
         [HttpPut]
-        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<Response<BrandDto>>> PutBrand([FromBody] BrandDto dto)
         {
             var res = new Response<BrandDto>();
-            Brand marca = new()
-            {
-                Id = dto.Id,
-                Name = dto.Name,
-                Description = dto.Description,
-                UpdatedDate = DateTime.Now,
-                UpdatedBy = Login,
-            };
-            marca = await _repository.UpdateAsync(marca);
+            dto = await _service.UpdateAsync(dto);
             /*Si dentro del método se cambió el Id a 0, es para indicar que el registro no existe o ya está eliminado*/
-            if (marca.Id == 0)
+            if (dto.Id == 0)
             {
                 res.Data = dto;
                 return NotFound();
             }
             return NoContent();
         }
+        /// <summary>
+        /// Realiza borrado lógico de la marca solicitada
+        /// </summary>
+        /// <param name="id">Identificador de la marca a eliminar</param>
+        /// <returns>Valor booleano que indica si se realizó el cambio de estatus correctamente</returns>
         [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<Response<bool>>> DeleteBrand(int id)
         {
-            var res = new Response<bool>();
-            res.Data = await _repository.DeleteAsync(id);
-
+            var res = new Response<bool>
+            {
+                Data = await _service.DeleteAsync(id)
+            };
             if (res.Data)
             {
                 return Ok(res);
             }
             return NotFound(res);
         }
+        #endregion
 
-        public string Login { get; set; } = "";
+        #region Propiedades
+        public string Login
+        {
+            get
+            {
+                /*Obtención de Id de usuario en sesión*/
+                Claim? claim = _contextAccessor.HttpContext?.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Sid);
+                return claim?.Value;
+            }
+        }
         #endregion
     }
 }
--- DeLaSalle.Ecommerce.Core/Dto/ProductCategoryDto.cs
+++ DeLaSalle.Ecommerce.Core/Dto/ProductCategoryDto.cs
@@ -1,9 +1,4 @@
 ﻿using DeLaSalle.Ecommerce.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DeLaSalle.Ecommerce.Core.Dto
 {
